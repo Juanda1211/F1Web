d97fbbb863a6638210eb21c5e6e59b5fbb1c687a
Juanda1211 <juandayclau1211@hotmail.com>
2025-11-06 12:39:05 -0500

    CV

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     { icon: Users, label: 'Drivers and teams', href: '/drivers' },
     { icon: MapPin, label: 'Tracks', href: '/tracks' },
     { icon: Trophy, label: 'History', href: '/history' },
-    { icon: Contact, label: 'Contact', href: 'https://portfolio-jd-1211.web.app/en' },
+    { icon: Contact, label: 'Contact', href: 'https://juanllanos-portfolio.xyz/en' },
   ];
 
   const toggleMobileMenu = () => setIsMobileOpen(!isMobileOpen);
